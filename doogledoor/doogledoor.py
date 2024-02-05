a6ab9e1d8d792427680a9a1e7cb64630770e3786
Jared Sulzdorf <j.sulzdorf@gmail.com>
2024-02-04 16:32:44 -0800

    wiring in DB calls and making front page

--- doogledoor/doogledoor.py
+++ doogledoor/doogledoor.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, render_template
+from doogledoor.db import database
+from doogledoor.model import DoogleDoor
+import sqlalchemy
 
 doog = Blueprint("doogledoor", __name__, template_folder="templates")
 
 
 @doog.route("/", methods=["GET"])
 def home():
+    with database.connect() as conn:
+        stmt = sqlalchemy.select(DoogleDoor).order_by(DoogleDoor.published)
+        for row in conn.execute(stmt).all():
+            print(row)
     return render_template("pages/home.html.jinja")
