eec8799b14af02bf15a12e2d6c717c87f6f209ac
Jared Sulzdorf <j.sulzdorf@gmail.com>
2024-02-06 10:35:07 -0800

    wiring in styling and getting default formatters working again

--- doogledoor/doogledoor.py
+++ doogledoor/doogledoor.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
-from doogledoor.db import database
-from doogledoor.model import DoogleDoor
+
+# from doogledoor.db import database
+# from doogledoor.model import DoogleDoor
 import sqlalchemy
 
 doog = Blueprint("doogledoor", __name__, template_folder="templates")
@@ -8,8 +9,44 @@ doog = Blueprint("doogledoor", __name__, template_folder="templates")
 
 @doog.route("/", methods=["GET"])
 def home():
-    with database.connect() as conn:
-        stmt = sqlalchemy.select(DoogleDoor).order_by(DoogleDoor.published)
-        for row in conn.execute(stmt).all():
-            print(row)
+
     return render_template("pages/home.html.jinja")
+
+
+daily_data = [
+    {"0:00": 0},
+    {"1:00": 0},
+    {"2:00": 0},
+    {"3:00": 0},
+    {"4:00": 0},
+    {"5:00": 0},
+    {"6:00": 2},
+    {"7:00": 2},
+    {"8:00": 0},
+    {"9:00": 0},
+    {"10:00": 4},
+    {"11:00": 3},
+    {"12:00": 2},
+    {"13:00": 4},
+    {"14:00": 0},
+    {"15:00": 0},
+    {"16:00": 5},
+    {"17:00": 6},
+    {"18:00": 5},
+    {"19:00": 2},
+    {"20:00": 2},
+    {"21:00": 0},
+    {"22:00": 0},
+    {"23:00": 0},
+    {"24:00": 0},
+]
+
+weekly_data = [
+    {"1/31": 20},
+    {"2/1": 30},
+    {"2/2": 35},
+    {"2/3": 36},
+    {"2/4": 40},
+    {"2/5": 22},
+    {"2/6": 22},
+]
--- doogledoor/static/scripts/doogledoor.tsx
+++ doogledoor/static/scripts/doogledoor.tsx
@@ -11,48 +11,66 @@ import {
   Rectangle,
 } from "recharts";
 
-const data = [
+let dailyData = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    hour: "6:00",
+    dd: 5,
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    hour: "7:00",
+    dd: 7,
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    hour: "8:00",
+    dd: 2,
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
+    hour: "9:00",
+    dd: 3,
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
+    hour: "10:00",
+    dd: 4,
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
+    hour: "11:00",
+    dd: 5,
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    hour: "12:00",
+    dd: 0,
+  },
+  {
+    hour: "13:00",
+    dd: 10,
+  },
+  {
+    hour: "14:00",
+    dd: 2,
+  },
+  {
+    hour: "15:00",
+    dd: 2,
+  },
+  {
+    hour: "16:00",
+    dd: 0,
+  },
+  {
+    hour: "17:00",
+    dd: 2,
+  },
+  {
+    hour: "18:00",
+    dd: 6,
+  },
+  {
+    hour: "19:00",
+    dd: 5,
+  },
+  {
+    hour: "20:00",
+    dd: 20,
   },
 ];
 
@@ -68,17 +86,60 @@ export default function DoogleDoor() {
 }
 
 function DoogleCount() {
-  return <h1>42</h1>;
+  return (
+    <div className="mb-5">
+      <h1 className="doogle-heading">42</h1>
+      <p className="">Today's Total</p>
+    </div>
+  );
 }
 
 function TimeSelectors() {
   return (
     <>
-      <div className="row row-cols-4">
-        <button>Today</button>
-        <button>This Week</button>
-        <button>This Month</button>
-        <button>This Year</button>
+      <div className="btn-group mb-5 btn-group-lg">
+        <input
+          type="radio"
+          className="btn-check"
+          name="btnradio"
+          id="btnradio1"
+          autoComplete="off"
+          checked
+        />
+        <label className="btn btn-outline-primary" htmlFor="btnradio1">
+          Today
+        </label>
+        <input
+          type="radio"
+          className="btn-check"
+          name="btnradio"
+          id="btnradio1"
+          autoComplete="off"
+        />
+        <label className="btn btn-outline-primary" htmlFor="btnradio1">
+          Past Week
+        </label>
+        <input
+          type="radio"
+          className="btn-check"
+          name="btnradio"
+          id="btnradio2"
+          autoComplete="off"
+        />
+        <label className="btn btn-outline-primary" htmlFor="btnradio2">
+          Past Month
+        </label>
+
+        <input
+          type="radio"
+          className="btn-check"
+          name="btnradio"
+          id="btnradio3"
+          autoComplete="off"
+        />
+        <label className="btn btn-outline-primary" htmlFor="btnradio3">
+          Past Year
+        </label>
       </div>
     </>
   );
@@ -86,32 +147,29 @@ function TimeSelectors() {
 
 function DoogleChart() {
   return (
-    <BarChart
-      width={500}
-      height={300}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar
-        dataKey="pv"
-        fill="#8884d8"
-        activeBar={<Rectangle fill="pink" stroke="blue" />}
-      />
-      <Bar
-        dataKey="uv"
-        fill="#82ca9d"
-        activeBar={<Rectangle fill="gold" stroke="purple" />}
-      />
-    </BarChart>
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart
+        width={500}
+        height={300}
+        data={dailyData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <XAxis dataKey="hour" />
+        <YAxis dataKey="dd" />
+        <Tooltip />
+        <Legend />
+        <Bar
+          dataKey="dd"
+          name="Doogle Ins and Outs"
+          fill="#61adc2"
+          activeBar={<Rectangle fill="#C29F61" />}
+        />
+      </BarChart>
+    </ResponsiveContainer>
   );
 }
